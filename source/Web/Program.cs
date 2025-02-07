a301d49d0f6aa93fd74cb8addd5ee1cdf43f6272
sDamme <sander.d.tamme@gmail.com>
2025-02-07 13:25:27 +0200

    Structure for DDD with frontend included

---
+++ source/Web/Program.cs
@@ -0,0 +1,25 @@
+using System.Runtime.Remoting.Contexts;
+
+var builder = WebApplication.CreateBuilder();
+
+builder.Host.UseSerilog();
+
+builder.Services.AddResponseCompression();
+builder.Services.AddJsonStringLocalizer();
+
+
+builder.Services.AddSwaggerDefault();
+builder.Services.AddControllers().AddJsonOptions();
+
+var application = builder.Build();
+
+application.UseException();
+application.UseHsts().UseHttpsRedirection();
+application.UseResponseCompression();
+application.UseStaticFiles();
+application.UseSwagger().UseSwaggerUI();
+application.UseRouting();
+application.MapControllers();
+application.MapFallbackToFile("index.html");
+
+application.Run();
