83b04c4c9384b26848486e0209c8382ab33b1adb
sDamme <sander.d.tamme@gmail.com>
2025-02-07 19:23:56 +0200

    Added Model project for DTOs

---
+++ source/Model/AttendeeModel.cs
@@ -0,0 +1,23 @@
+﻿namespace EventNet.Model
+{
+    public abstract record AttendeeModel
+    {
+        public long Id { get; init; }
+        public string PaymentType { get; init; }
+        public string Description { get; init; }
+    }
+
+    public sealed record InidividualAttendeeModel : AttendeeModel
+    {
+        public string FirstName { get; init; }
+        public string LastName { get; init; }
+        public string PersonalIdCode { get; init; }
+    }
+
+    public sealed record BusinessAttendeeModel : AttendeeModel
+    {
+        public string LegalName { get; init; }
+        public string RegistrationCode { get; init; }
+        public int NumberOfAttendees { get; init; }
+    }
+}
---
+++ source/Model/EventModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventNet.Model
+{
+    public sealed record EventModel
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public DateTime EventDate { get; set; }
+        public string Location { get; set; }
+        public string Description { get; set; }
+        public List<AttendeeModel> Attendees { get; set; } = [];
+    }
+}
